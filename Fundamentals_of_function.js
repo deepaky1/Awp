0cba3c368ee7531dd2710bf187c83fc1269289fb
deepaky1 <68921546+deepaky1@users.noreply.github.com>
2020-08-12 23:59:16 +0530

    Add files via upload

---
+++ Fundamentals_of_function.js
@@ -0,0 +1,22 @@
+
+/**Fundamentals of function 
+rollno:-200240520031
+name:-Deepak A Yadav */
+
+//Normal function calling //
+function check(){
+
+    console.log("Hello Every One")
+};
+
+//arrow function calling//
+
+let check = ()=> {
+    console.log("Testing Purpose lamda function")
+};
+
+//annonymous Function  calling//
+let check =function(){
+console.log("Anonmous function call")
+
+};
